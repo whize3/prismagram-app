403533d93a3d36cec20de91f65b98de739b2b720
정후영 <whize3@gmail.com>
2019-10-28 22:19:04 +0900

    navigation

---
+++ components/NavController.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
+import AuthNavigation from "../navigation/AuthNavigation";
+
+export default () => {
+  const isLoggedIn = useIsLoggedIn();
+  const logIn = useLogIn();
+  const logOut = useLogOut();
+  return (
+    <View style={{ flex : 1}}>
+      {isLoggedIn ? (
+        <TouchableOpacity onPress={logOut}>
+          <Text>Log Out</Text>
+        </TouchableOpacity>
+      ) : (
+        <AuthNavigation />
+      )}
+    </View>
+  );
+};
